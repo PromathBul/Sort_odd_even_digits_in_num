2bd21e8982c06e8257b3516bcf8deea6f1bb7944
Dmitry_Tikhonov <tdmv1209@gmail.com>
2023-08-21 17:44:45 +0300

    Resolved

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿int InputNum (string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SortingDigits(int num)
+{
+    int right = 0;
+    int left = 0;
+    int evens = 0;
+    int odds = 0;
+    while (num != 0)
+    {
+        int digit = num % 10;
+        if (num % 2 == 0)
+        {
+            evens += digit * (int)Math.Pow(10, right);
+            right++;
+        }
+        else
+        {
+            odds += digit * (int)Math.Pow(10, left);;
+            left++;
+        }
+        num /= 10;
+    }
+    int res = odds * (int)Math.Pow(10, right) + evens;
+    return res;
+}
+
+int num = InputNum("Введите число: ");
+System.Console.WriteLine(SortingDigits(num));
